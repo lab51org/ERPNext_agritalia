359b17e771e9435f2eb8ce03e65df710f2925bf2
Matteo D <md@lab51.org>
2021-03-16 16:03:25 +0100

    added Campo and Coltivazione DocType

---
+++ agrierp/anagrafiche/doctype/coltivazione/coltivazione.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Lab51.org and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Coltivazione', {
+	// refresh: function(frm) {
+
+	// }
+});
